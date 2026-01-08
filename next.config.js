b8e97026b5d9bdbbf67efc46e2e050c485c82c68
Jahanbani Ardakani <alij@iastate.edu>
2026-01-08 17:48:46 -0600

    Configure for static export

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
-    formats: ['image/avif', 'image/webp'],
+    unoptimized: true,
   },
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
